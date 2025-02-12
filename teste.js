3fc0eabf69557feb9297e27757e6e780ca0f2443
João Fidelis <jfidelis999@gmail.com>
2025-02-12 19:52:33 -0300

    Create teste.js

---
+++ teste.js
@@ -0,0 +1,42 @@
+import {Text, View, StyleSheet} from 'react-native'
+
+export default function App(){
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text0}>olá Gru, hoje tem lua Cheia!</Text>
+      <Text style={styles.text1}>Roger guedes saiu do cortinas</Text>
+      <Text style={styles.text2}>não não e uma frase</Text>
+      </View>
+  );
+
+}
+// !frkljfuorhvpijrfuohroifhcuiedhfoicd
+//# ufhuehfiod
+//? eifhiojd
+//TODO: eifhiojd
+const styles = StyleSheet.create({
+  container: {
+    backgroundimage: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Ffreefrontend.com%2Fjavascript-background-effects%2F&psig=AOvVaw3PTChADEJGzAxCoUGyy6h8&ust=1739402285610000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIiqqKDhvIsDFQAAAAAdAAAAABAJ',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F0E68C',
+    
+  text0: {
+    
+    color: '#003366',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  text1: {
+    color: 'gray',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  text2: {
+    color: 'red',
+    fontSize: 24,
+    fontWeight: 'bold',
+    
+  },
+});
